da5fb8cb5d655292211fb62153d8d670c4d4c858
Alaa Alassi <alaa.alasi1989@gmail.com>
2022-05-21 18:35:54 +0200

    search function works

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -3,18 +3,19 @@
 
 #include "../../render/render.h"
 
-
+using namespace std;
 // Structure to represent node of kd tree
 struct Node
 {
 	std::vector<float> point;
 	int id;
-	Node* left;
-	Node* right;
+	Node *left;
+	Node *right;
 
 	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
-	{}
+		: point(arr), id(setId), left(NULL), right(NULL)
+	{
+	}
 
 	~Node()
 	{
@@ -25,81 +26,90 @@ struct Node
 
 struct KdTree
 {
-	Node* root;
-	Node *currentNode = NULL;
+	Node *root;
 
 	KdTree()
-	: root(NULL)
-	{}
+		: root(NULL)
+	{
+	}
 
 	~KdTree()
 	{
 		delete root;
 	}
 
-void insertHelper(Node **node,unsigned int depth,std::vector<float> point, int id)
-   {
-	int changeDimention = depth%2;
+	void insertHelper(Node **node, unsigned int depth, std::vector<float> point, int id)
+	{
+		int changeDimention = depth % 2;
 
-      if(*node == NULL)
-      {
-        *node = new Node(point,id);
-      }
+		if (*node == NULL)
+		{
+			*node = new Node(point, id);
+		}
 
-      else if(point[changeDimention] < (*node)->point[changeDimention])
-      {
-        insertHelper(&(*node)->left,depth+1,point,id);
-      }
-      else
-      {
-        insertHelper(&(*node)->right,depth+1,point,id);
-      }
-   }
+		else if (point[changeDimention] < (*node)->point[changeDimention])
+		{
+			insertHelper(&(*node)->left, depth + 1, point, id);
+		}
+		else
+		{
+			insertHelper(&(*node)->right, depth + 1, point, id);
+		}
+	}
 
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// the function should create a new node and place correctly with in the root
 
-		insertHelper(&root,0,point,id);
-
+		insertHelper(&root, 0, point, id);
 	}
 
-	void searchHelper(){
+	void searchHelper(vector<float> target, Node *node, int depth, float distanceTolerance, vector<int> &ids)
+	{
 
+		if (node != NULL)
+		{
+			float Px = target[0] - node->point[0];
+			float Py = target[1] - node->point[1];
+			bool isInsideTheBox = abs(Px) <= distanceTolerance && abs(Py) <= distanceTolerance;
+			unsigned int depth = 0;
+			int changeDimention = depth%2;
+			/*std::cout << "root " << currentNode->point[0] << " " << currentNode->point[1] << std::endl;
+			std::cout << "target " << target[0] << " "<< target[1] << std::endl;
+			std::cout << "isInsideTheBox " << isInsideTheBox << std::endl;*/
+
+			if (isInsideTheBox)
+			{
+				float d = sqrt(Px * Px + Py * Py);
+				std::cout << "d " << d << std::endl;
+				if (d <= distanceTolerance)
+				{
+					ids.push_back(node->id);
+				}
+			}
+
+			//search the left branch
+			auto rightArrow = (target[changeDimention]+distanceTolerance);
+			bool searchRightBranch = rightArrow > node->point[changeDimention];
+			if(searchRightBranch){
+				cout << "searchRightBranch" << endl;
+				searchHelper(target,node->right,depth+1,distanceTolerance,ids);
+			}
+			auto leftArrow = (target[changeDimention]-distanceTolerance);
+			bool searchLeftBranch = leftArrow < node->point[changeDimention];
+			if(searchLeftBranch){
+				cout << "searchLeftBranch" << endl;
+				searchHelper(target,node->left,depth+1,distanceTolerance,ids);
+			}
+		}
 	}
 
 	// return a list of point ids in the tree that are within distance of target
 	std::vector<int> search(std::vector<float> target, float distanceTol)
 	{
-		if(currentNode == NULL){
-			currentNode = root;
-		}
 		std::vector<int> ids;
-		float Px = target[0] - currentNode->point[0];
-		float Py = target[1] - currentNode->point[1];
-		bool isInsideTheBox = abs(Px) <= distanceTol && abs(Py) <= distanceTol;
-		unsigned int depth =0;
-        bool isOnRight = target[0] + distanceTol > 0;
-		std::cout << "root " << currentNode->point[0] << " " << currentNode->point[1] << std::endl;
-		std::cout << "target " << target[0] << " "<< target[1] << std::endl;
-		std::cout << "isInsideTheBox " << isInsideTheBox << std::endl;
-		if(isInsideTheBox){
-			float d = sqrt(Px*Px + Py*Py);
-			std::cout << "d " << d << std::endl;
-			if(d <= distanceTol){
-				ids.push_back(currentNode->id);
-			}
-
-
-		}
-
+		searchHelper(target, root, 0, distanceTol, ids);
 		return ids;
 	}
-	
-
 };
-
-
-
-
